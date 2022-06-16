0481c092b6f4e9cddc6a641a520f021e03e58560
dipprajapati13 <103754967+dipprajapati13@users.noreply.github.com>
2022-06-16 20:08:14 +0530

    dfb
    hth

---
+++ script.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.portfolio .button-container .btn').click(function(){
+
+    let filter = $(this).attr('data-filter');
+
+    if(filter == 'all'){
+      $('.portfolio .image-container .box').show('400')
+    }else{
+      $('.portfolio .image-container .box').not('.'+filter).hide('200');
+      $('.portfolio .image-container .box').filter('.'+filter).show('400');
+    }
+
+  });
+
+  $('#theme-toggler').click(function(){
+    $(this).toggleClass('fa-sun');
+    $('body').toggleClass('dark-theme');
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
+var words = [ 'hello','I Am', 'Dip prajapati', ' BACHLOUR OF COMPUTER APPLICATON STUDENT / WEB DESIGNER'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
